c017ee244c1770f029e2c8a44b7774baa39429ab
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2024-10-17 14:47:27 +0800

    changes to main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Load the image
-image_path = "new_imgs/img3.jpg"  # Change this to the path of your image
+image_path = "new_imgs/img4.jpg"  # Change this to the path of your image
 # image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 image = cv2.imread(image_path)
 
@@ -27,8 +27,7 @@ thresh = cv2.inRange(image, lower, upper)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
 morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-cv2.imshow("image", image)
-cv2.imshow("morph", morph)
+cv2.imwrite("morph.jpg", morph)
 
 # Set up the blob detector with parameters tuned for pill detection
 params = cv2.SimpleBlobDetector_Params()
@@ -39,16 +38,16 @@ params.minArea = 300  # Adjusted for the image resolution and pill size
 params.maxArea = 100000000  # Tuned to ignore overly large objects
 
 # Filter by circularity: Pills are often round or oval
-params.filterByCircularity = True
-params.minCircularity = 0.6  # Set lower to accommodate oval pills
+params.filterByCircularity = False
+# params.minCircularity = 0.6  # Set lower to accommodate oval pills
 
 # Filter by convexity: Pills typically have a convex shape
-params.filterByConvexity = True
-params.minConvexity = 0.7  # Can be adjusted for different pill shapes
+params.filterByConvexity = False
+# params.minConvexity = 0.7  # Can be adjusted for different pill shapes
 
 # Filter by inertia: Helps to detect elongated objects like capsules
-params.filterByInertia = True
-params.minInertiaRatio = 0.1  # Allows detection of both circular and elongated pills
+params.filterByInertia = False
+# params.minInertiaRatio = 0.1  # Allows detection of both circular and elongated pills
 
 # Set up the detector with the tuned parameters
 detector = cv2.SimpleBlobDetector_create(params)
@@ -68,8 +67,4 @@ def draw_keypoints(img, keypoints, color):
 print(len(keypoints))
 
 draw_keypoints(morph, keypoints, (255, 0, 0))
-
-# Show the output image with detected pills
-cv2.imshow("Detected Pills", morph)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imshow("annotated.jpg", morph)
